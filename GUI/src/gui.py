cfe6a52658135aca11c9183d1dac200c496ebdde
Sean Alling <sean.alling@gmail.com>
2019-12-11 23:51:12 -0800

    Main GUI python interface
    gui.py defines the main function for setting up and running the GUI.
    gui.py defines the subclass of the GUI that is generated from
    wxFormBuilder.
    
    This file also defines all events that should be processed by the GUI.
    The class generated by wxFormBuilder provides empty functions this file
    is where the actual implementation of these functions takes place.

---
+++ GUI/src/gui.py
@@ -0,0 +1,35 @@
+#importing wx files
+import wx
+import wx.adv
+import wx.dataview as dv
+
+import os
+
+import BOM_Tool
+from wx.lib.floatcanvas import FloatCanvas
+ 
+# Extend the gui with some new functionality
+class MyFrame(BOM_Tool.MainFrame):
+    def __init__(self, parent):
+        BOM_Tool.MainFrame.__init__(self, parent)
+
+    # Virtual event handlers, overide them in your derived class
+    def onMove( self, event ):
+        """
+        Updates the status bar with the world coordinates
+        """
+        self.SetStatusText("%.2g, %.2g"%tuple(event.Coords))
+
+
+
+####################################################################################################
+
+def main():
+    app = wx.App()
+    frame = MyFrame(None)
+    frame.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
