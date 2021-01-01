3b426546124e62bb35700d79f78a23cd9454cd01
oceanofthelost <sean.alling@gmail.com>
2020-12-31 22:45:59 -0800

    Fixed bug with via rendering
    When a via is a polygon shape, the size of the via was being rendered
    smaller if a polygon. This was due to the the assumption that the
    polygon was inscribed with a circle, where really the polygon
    circumscribes the circle.
    
    This commit now calculates the radius that would make the polygon
    inscribed. The radius is then used with the original formula in order
    to calculate the points of the polygon.

--- GUI/src/render/render_via.js
+++ GUI/src/render/render_via.js
@@ -7,9 +7,17 @@ function GetPolygonVerticies(radius, numberSized)
     // Will store the verticies of the polygon.
     let polygonVerticies = [];
     // Assumes a polygon centered at (0,0)
+    // Assumes that a circumscribed polygon. The formulas used belo are for a inscribed polygon. 
+    // To convert between a circumscribed to an inscribed polygon, the radius for the outer polygon needs to be calculated.
+    // Some of the theory for below comes from 
+    // https://www.maa.org/external_archive/joma/Volume7/Aktumen/Polygon.html
+    // // Its is some basic trig and geometry
+    let alpha = (2*Math.PI / (2*numberSized));
+    let inscribed_radius = radius /Math.cos(alpha)
     for (let i = 1; i <= numberSized; i++) 
     {
-        polygonVerticies.push(new Point(radius * Math.cos(2 * Math.PI * i / numberSized), radius * Math.sin(2 * Math.PI * i / numberSized)));
+
+        polygonVerticies.push(new Point(inscribed_radius * Math.cos(2 * Math.PI * i / numberSized), inscribed_radius * Math.sin(2 * Math.PI * i / numberSized)));
     }
 
     return polygonVerticies;
